3df9e3f83e5474cfc33bafeb72b95263613799c6
Sebaplz <sebastian.h.neira@outlook.com>
2024-07-22 21:33:39 -0400

    fix url

--- src/api/omdbApi.ts
+++ src/api/omdbApi.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from "axios";
 import { OmdbApiInfoResponse, OmdbApiResponse } from "./interfaces";
 
 const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
-const API_URL = "http://www.omdbapi.com";
+const API_URL = "https://www.omdbapi.com";
 
 const instance: AxiosInstance = axios.create({
   baseURL: API_URL,
